fcfbc2796793d5dce2ab4203f540e30832443f6e
Anatoliy Svyrydenko <anatoliy.svyrydenko@gmail.com>
2019-07-23 23:41:14 +0300

    + add small Cities game

---
+++ main.cpp
@@ -0,0 +1,35 @@
+#include <iostream>
+#include <string>
+#include <vector>
+
+using namespace std;
+
+int main()
+{
+    vector<string> cities = {"Kyiv", "Kharkiv", "Vinnytsia", "Sumy", "Yevpatoria", "Yalta"};
+
+    bool continueGame = true;
+
+    while (continueGame) {
+        string city;
+        cout << "Enter city:" << endl;
+        cin >> city;
+
+        char inputLastChar = city.back();
+
+        continueGame = false;
+        for(vector<string>::const_iterator currCity = cities.begin(); currCity != cities.end(); ++currCity) {
+            char fChar = tolower((*currCity).at(0));
+
+            if (fChar == inputLastChar) {
+                cout << "My answer is: " << *currCity << endl;
+                cities.erase(currCity);
+                continueGame = true;
+                break;
+            }
+        }
+    }
+
+    cout << "Oh no! I'm defeated!" << endl;
+    return 0;
+}
